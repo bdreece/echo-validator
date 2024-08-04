0f758518f29d5075e8ed9995d886ca0f3e8a3b11
Brian Reece <bdreece@mtu.edu>
2024-08-04 15:51:29 -0400

    docs: update doc comments, README.md
    rename CI workflow

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![.github/workflows/ci.yml](https://github.com/bdreece/echo-validator/actions/workflows/ci.yml/badge.svg)](https://github.com/bdreece/echo-validator/actions/workflows/ci.yml)
+[![Go Reference](https://pkg.go.dev/badge/github.com/bdreece/echo-validator.svg)](https://pkg.go.dev/github.com/bdreece/echo-validator)
+
 # echo-validator
 
 A basic implementation of [echo.Validator](https://pkg.go.dev/github.com/labstack/echo/v4#Validator)
--- validator.go
+++ validator.go
@@ -2,18 +2,25 @@ package echovalidator
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
 )
 
+// The [validator.Validate] wrapper.
 type Validator struct {
     validator *validator.Validate
 }
 
+// Validate implements [echo.Validator]
 func (v *Validator) Validate(i any) error {
     return v.validator.Struct(i)
 }
 
+// Creates a new [Validator].
 func New() *Validator {
     return &Validator{validator.New()}
 }
 
+// A global singleton [Validator].
 var Default = New()
+
+var _ echo.Validator = (*Validator)(nil)
